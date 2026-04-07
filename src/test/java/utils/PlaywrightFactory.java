fd7fbf50d2da3d116f216c352bea67830b124b00
Saranya-P14 <saranyapathipaticsd14@gmail.com>
2026-04-07 09:12:38 +0530

    Update PlaywrightFactory.java

--- src/test/java/utils/PlaywrightFactory.java
+++ src/test/java/utils/PlaywrightFactory.java
@@ -1,1 +1,17 @@
+package utils;
 
+import com.microsoft.playwright.*;
+
+public class PlaywrightFactory {
+
+    public static Playwright initPlaywright() {
+        return Playwright.create();
+    }
+
+    public static Browser launchBrowser(Playwright playwright) {
+        return playwright.chromium().launch(
+            new BrowserType.LaunchOptions().setHeadless(false)
+        );
+    }
+}
+//Central place for browser config

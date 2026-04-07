056b06596c535b2ea4253ee3bf288cd85bd3850b
Saranya-P14 <saranyapathipaticsd14@gmail.com>
2026-04-07 09:08:26 +0530

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,1 +1,23 @@
+package base;
 
+import com.microsoft.playwright.*;
+import utils.PlaywrightFactory;
+
+public class BaseTest {
+    protected Playwright playwright;
+    protected Browser browser;
+    protected Page page;
+
+    public void setUp() {
+        playwright = PlaywrightFactory.initPlaywright();
+        browser = PlaywrightFactory.launchBrowser(playwright);
+        page = browser.newPage();
+    }
+
+    public void tearDown() {
+        browser.close();
+        playwright.close();
+    }
+}
+
+//Handles:Browser launch, Page initialization, Cleanup

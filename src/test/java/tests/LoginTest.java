aec778a0722bd7671c2dbad1b5d0f2d7ea1c5410
Saranya-P14 <saranyapathipaticsd14@gmail.com>
2026-04-07 09:11:09 +0530

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,1 +1,28 @@
+package tests;
 
+import base.BaseTest;
+import org.testng.annotations.*;
+import pages.LoginPage;
+import utils.ConfigReader;
+
+public class LoginTest extends BaseTest {
+
+    LoginPage loginPage;
+
+    @BeforeMethod
+    public void setupTest() {
+        setUp();
+        loginPage = new LoginPage(page);
+    }
+
+    @Test
+    public void verifyLogin() {
+        loginPage.navigate(ConfigReader.get("url"));
+        loginPage.login("admin", "password");
+    }
+
+    @AfterMethod
+    public void tearDownTest() {
+        tearDown();
+    }
+}
